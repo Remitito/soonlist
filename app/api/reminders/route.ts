fd5b48ccb2abbc6a11c2d307028a400869816604
Remitito <jrossOT@protonmail.com>
2025-08-08 20:57:48 +0100

    Added vercel cron jobs

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -15,7 +15,11 @@ interface TaskReminderInfo {
   dueReminders: string[];
 }
 
-export async function GET() {
+export async function GET(request: Request) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
   await dbConnect();
 
   const tasks = await Task.find({
