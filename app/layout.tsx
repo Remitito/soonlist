bbaafed234cf9749ab619feeaa52265f7c5a1ebd
Remitito <jrossOT@protonmail.com>
2025-08-10 13:04:54 +0100

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "./fonts";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Deadline Desk",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${inter.className} text-main-text bg-background antialiased w-full overflow-x-hidden flex justify-center flex-col`}
       >
         <div className="flex w-full flex-col">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
